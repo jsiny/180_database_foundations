bc5f79c34bb596f72a471b3864e0555c555b0889
Juliette Sinibardy <juju.siny@gmail.com>
2019-12-05 15:03:42 +0100

    :construction: Start ex 6.1

---
+++ ex/6_medium_2/1.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE auction;
+
+CREATE TABLE bidders (
+  id serial PRIMARY KEY,
+  name text NOT NULL
+);
+
+CREATE TABLE items (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  initial_price numeric(6,2) NOT NULL CHECK (initial_price BETWEEN 0.01 AND 1000.00),
+  sales_price numeric(6,2) CHECK (initial_price BETWEEN 0.01 AND 1000.00)
+);
+
+CREATE TABLE bids (
+  id serial PRIMARY KEY,
+  bidder_id int NOT NULL REFERENCES bidders (id) ON DELETE CASCADE,
+  item_id   int NOT NULL REFERENCES items (id)   ON DELETE CASCADE,
+  amount numeric(6,2) NOT NULL CHECK (amount BETWEEN 0.01 AND 1000.00) 
+);
+
+CREATE INDEX ON bids (bidder_id, item_id);
+
+\copy bidders FROM 'bidders.csv' WITH HEADER CSV;
